9fd48f225c01b0b8badb9dbe338646ce511723d5
Kota Karthikeya Raj <24311a04eg@ece.sreenidhi.edu.in>
2026-05-22 16:46:00 +0530

    Add setup.py to make app package importable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-ci-template',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+        'flask-sqlalchemy',
+        'flask-jwt-extended',
+        'flask-cors',
+        'python-dotenv',
+    ],
+)
